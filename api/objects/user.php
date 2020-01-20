d8f369a3be383b295c187c47a91a53b73076340b
giraldokarol <karol.giraldo9505@gmail.com>
2020-01-20 14:45:04 +0100

    Update user/readOrder function query

--- api/objects/user.php
+++ api/objects/user.php
@@ -75,7 +75,7 @@
         }
 
         function readOrder(){
-            $query = "SELECT * FROM user AS u INNER JOIN order AS o INNER JOIN product AS p
+            $query = "SELECT * FROM user AS u INNER JOIN heroku_1ace2ff26d7733b.order AS o INNER JOIN product AS p
             ON u.idUser=o.User_idUser AND o.Product_idProduct=p.idProduct AND u.idUser=?";
             $stmt= $this->conn->prepare($query);
             $stmt->bindParam(1, $this->idUser);
