6ed170b25507cd147633a88078f94a98f2c65274
Karol Giraldo Escobar <karol.giraldo9505@gmail.com>
2025-03-10 14:16:53 +0100

    fix: Control allow methods and allow header in login.php

--- api/user/login.php
+++ api/user/login.php
@@ -1,9 +1,14 @@
 <?php
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
-    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Allow-Methods: POST, OPTIONS");
     header("Access-Control-Max-Age: 3600");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Access-Control-Allow-Headers");
+
+	if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        http_response_code(200); 
+        exit();
+    }
 
     include_once '../config/database.php';
     include_once '../objects/user.php';
